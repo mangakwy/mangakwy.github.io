c6e0b9601a6b09dd7ca14a203b246f972208df3a
Kai-TRD <wkailuo03@gmail.com>
2023-05-19 20:26:36 +0200

    a

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# mangakwy.github.io
+![alt text](https://github.com/Kai-TRD/DAM/blob/main/Lenguaje_de_marcas/mangak/images/banners/banner2.jpg?raw=true)
+# Mangak
+Proyecto de lenguaje de marcas
+## Autor
+* Kai
+## Fuentes
+* [LectorTMO](https://lectortmo.com/)
+* [NineManga](https://my.ninemanga.com/)
+* [Olympus Scans](https://olympusscans.com/)
+* [Phoenix Fansub](https://phoenixfansub.com/)
---
+++ js/signIn.js
@@ -0,0 +1,72 @@
+
+// var account = {
+//     username: "",
+//     email: "",
+//     password: "",
+//     notification: false,
+//     tyc: false,
+// }
+
+// var username = document.getElementById("username");
+// var email = document.getElementById("email");
+// var password = document.getElementById("password");
+// var notification = document.getElementById("notification");
+// var tyc = document.getElementById("tyc");
+
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.querySelector('form');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        // guardar el userName
+        var nombreUsuarioElemento = document.getElementById('nombreUsuario');
+        var prueba = document.getElementById('javascriptForm');
+
+
+        // obtener valores del form
+        var username = document.getElementById('username').value;
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        var passwordRepit = document.getElementById('passwordRepit').value;
+        var tycCheckbox = document.getElementById('tyc');
+
+        // comprobaciones
+        if (!username || !email || !password || !passwordRepit) {
+            alert('Por favor, complete todos los campos del formulario.');
+            return;
+        }
+
+        if (password !== passwordRepit) {
+            alert('Las contraseñas no coinciden.');
+            return;
+        }
+
+        if (!tycCheckbox.checked) {
+            alert('Debe aceptar los términos y condiciones.');
+            return;
+        }
+
+
+        var account = {
+            username: username,
+            email: email,
+            password: password,
+            // notification: false,
+            tyc: tycCheckbox,
+        };
+
+        // alert(prueba)
+
+        var prueba = JSON.stringify(account);
+        document.getElementById('jsonContainer').textContent = prueba;
+
+
+        
+
+        nombreUsuarioElemento.textContent = username;
+        var elementoValido = document.querySelector('.valido');
+        elementoValido.style.display = 'block';
+
+        form.reset();
+    });
+});
